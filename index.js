122da372048862b1a3e7aa728641c1511bc9093c
Sean Chan <dcchan98@gmail.com>
2021-02-16 15:01:20 +0800

    update package.json for heroku

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+  console.log(req)
   res.send({
     msg: "hello"
   })
 })
 
-app.get('/bye', (req, res) => {
-  res.send({
-    msg: "bye"
-  })
-})
-
-app.listen(5000)
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
 
 console.log("listening")
